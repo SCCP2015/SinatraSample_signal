ae0ce80c3fbe48e1afcb0ff6cf52767aadd1950b
signal0801 <s1220073@gmail.com>
2015-06-18 03:47:49 +0900

    add erb

--- src/app.rb
+++ src/app.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 require 'sinatra/base'
-require 'sinatra/json'
 require 'sinatra/reloader'
 require 'data_mapper'
 require_relative 'word'
+require 'erb'
 
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, 'postgres://vagrant:vagrant@localhost/myapp')
@@ -15,37 +15,41 @@ class MainApp < Sinatra::Base
     register Sinatra::Reloader
   end
   get '/words' do
-    json(Word.all)
+    words = Word.all.map do |w|
+      w.id.to_s + ": #{w.msg}"
+    end
+    words.join(', ')
   end
   get '/words/:id' do
     id = params[:id]
     word = Word.get(id)
     if word.nil?
-      json(error: "id:#{id} is not found.")
+      "Record of id: #{id} is not found."
     else
-      json(word)
+      word.id.to_s + ": #{word.msg}"
     end
   end
   post '/words' do
-    Word.create(msg: request.body.gets).id.to_json
+    word = Word.create(msg: request.body.gets)
+    word.id.to_s
   end
   put '/words/:id' do
     id = params[:id]
     word = Word.get(id)
     if word.nil?
-      json(false)
+      'false'
     else
       word.update(msg: request.body.gets)
-      json(true)
+      'true'
     end
   end
   delete '/words/:id' do
     id = params[:id]
     word = Word.get(id)
     if word.nil?
-      json(false)
+      'false'
     else
-      json(word.destroy)
+      word.destroy.to_s
     end
   end
 end
